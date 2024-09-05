ac07c7fcfd72d0cacb9fa1664c29403ddc983036
MarkMallari16 <mallariftw01@gmail.com>
2024-09-05 14:09:34 +0800

    fix ui of blog card

--- resources/js/Components/BlogCard.vue
+++ resources/js/Components/BlogCard.vue
@@ -1,9 +1,67 @@
-<script setup></script>
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+
+defineProps({
+  post: {
+    type: Object,
+    required: true,
+  },
+  authUserId: {
+    type: Number,
+    required: true,
+  },
+});
+const form = useForm({
+  title: "",
+  content: "",
+  image: null,
+});
+
+const deletePost = (postId) => {
+  if (confirm("Are you sure you want to delete this post?")) {
+    form.delete(route("post.delete", postId));
+  }
+};
+</script>
 <template>
+  <div>
     <div>
-        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.forbes.com%2Fadvisor%2Fbusiness%2Fstart-a-blog%2F&psig=AOvVaw2CJvK7iA7DU5bW03PcjhGw&ust=1724829925196000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjH5NjRlIgDFQAAAAAdAAAAABAE" alt="blog image">
+      <span class="text-slate-400">Artificial Intelligence</span>
+      <img
+        class="mt-3 rounded-lg w-full object-cover"
+        :src="`/storage/${post.image}`"
+        :alt="post.title"
+      />
     </div>
-    <div>
-        <h1 class="text-4xl">Sample title blog</h1>
+
+    <h1 class="text-2xl mt-4 font-bold">{{ post.title }}</h1>
+
+    <p>
+      {{ post.content }}
+    </p>
+
+    <div class="mt-5 justify-between flex items-center">
+      <div class="flex items-center gap-3">
+        <img :src="`/storage/avatars/${post.user.avatar}`" alt="avatar" class="w-8" />
+        <p>{{ post.user.name }}</p>
+      </div>
+      <div>
+        <button class="bg-primary text-white rounded-lg p-2 flex  items-center gap-2">
+          Read Full Article
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="size-5"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
+  </div>
 </template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,7 @@
 <script setup>
+import BlogCard from "@/Components/BlogCard.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head, useForm } from "@inertiajs/vue3";
+import { Head } from "@inertiajs/vue3";
 
 defineProps({
   posts: {
@@ -12,44 +13,20 @@ defineProps({
     required: true,
   },
 });
-const form = useForm({
-  title: "",
-  content: "",
-  image: null,
-});
-const deletePost = (postId) => {
-  if (confirm("Are you sure you want to delete this post?")) {
-    form.delete(route("post.delete", postId));
-  }
-};
+
 </script>
 
 <template>
   <Head title="Dashboard" />
 
   <AuthenticatedLayout>
-    <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
-    </template>
-
-    <div class="mt-10 mx-5 lg:mx-48" v-if="posts && posts.length > 0">
-      <div v-for="post in posts" :key="post.id" class="bg-white mb-4 p-5 rounded-lg">
-        <div class="flex justify-between items-center">
-          <h3 class="text-xl font-medium">{{ post.user.name }}</h3>
-          <button
-            v-if="post.user_id === authUserId"
-            @click="deletePost(post.id)"
-            class="bg-red-500 text-white p-2 rounded-lg"
-          >
-            Delete
-          </button>
-        </div>
-        <h1 class="text-4xl mt-4 font-bold">{{ post.title }}</h1>
-        <img
-          class="mt-4 rounded-lg w-96"
-          :src="`/storage/${post.image}`"
-          :alt="post.title"
-        />
+    <div class=" mt-10 mx-5 lg:mx-48 gap-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3" v-if="posts && posts.length > 0">
+      <div
+        v-for="post in posts"
+        :key="post.id"
+        class="mb-4 bg-white px-5 py-6 rounded-xl "
+      >
+        <BlogCard :post="post" :authUserId="authUserId" />
       </div>
     </div>
     <div v-else class="mt-10 text-center">
