e2f5b7686cc3e8141bbaa4b3c676c9a1a0296fba
MarkMallari16 <mallariftw01@gmail.com>
2024-09-11 05:40:40 +0800

    add welcome image for temporary

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -22,12 +22,11 @@ defineProps({
 <template>
   <Head title="Welcome" />
   <div class="relative sm:flex sm:items-center sm:justify-between p-8">
-    <h1 class="text-xl font-black uppercase">
+    <h1 class="text-2xl font-black uppercase">
       Tambayan<span class="text-orange-500">Thoughts</span>
     </h1>
 
-    <div>
-      <div v-if="canLogin">
+    <div v-if="canLogin">
         <Link
           v-if="$page.props.auth.user"
           :href="route('dashboard')"
@@ -36,35 +35,42 @@ defineProps({
           Dashboard
         </Link>
 
-       
         <template v-else>
-            <div class="flex gap-5">
-              <Link
-                :href="route('login')"
-                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm"
-              >
-                Blogs
-              </Link>
-              <Link
-                :href="route('login')"
-                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm"
-              >
-                Log in
-              </Link>
-  
-              <Link
-                v-if="canRegister"
-                :href="route('register')"
-                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm"
-              >
-                Register
-              </Link>
-            </div>
-          </template>
+          <div class="flex gap-5">
+            <Link
+              :href="route('login')"
+              class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm"
+            >
+              Blogs
+            </Link>
+            <Link
+              :href="route('login')"
+              class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm"
+            >
+              Log in
+            </Link>
+
+            <Link
+              v-if="canRegister"
+              :href="route('register')"
+              class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm"
+            >
+              Register
+            </Link>
+          </div>
+        </template>
       </div>
-    
-    </div>
-   
   </div>
-  <h1 class="p-8 mt-36 text-4xl font-bold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, alias quisquam aliquam tempora tempore velit? Reiciendis enim recusandae neque similique.</h1>
+  
+  <div class="flex items-center flex-wrap lg:flex-nowrap gap-4 p-8 ">
+    <h1 class="text-4xl font-bold">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, alias quisquam
+      aliquam tempora
+    </h1>
+    <img
+      src="https://img.freepik.com/free-photo/teamwork-meeting-with-business-people_23-2148825936.jpg?t=st=1726004089~exp=1726007689~hmac=f85fec63a6b5c07d31a038a20dab01a2a7c6efb907bfc01958a93b53f1276fc0&w=900"
+      alt="blog-picture"
+      class="rounded-sm"
+    />
+  </div>
 </template>
