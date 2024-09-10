cfb6c97d7358dc10c1ec40ae47e37d441275996d
MarkMallari16 <mallariftw01@gmail.com>
2024-09-11 05:32:56 +0800

    add blog website name in welcome

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,55 +1,70 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
+import { Head, Link } from "@inertiajs/vue3";
 
 defineProps({
-    canLogin: {
-        type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    },
-    laravelVersion: {
-        type: String,
-        required: true,
-    },
-    phpVersion: {
-        type: String,
-        required: true,
-    },
+  canLogin: {
+    type: Boolean,
+  },
+  canRegister: {
+    type: Boolean,
+  },
+  laravelVersion: {
+    type: String,
+    required: true,
+  },
+  phpVersion: {
+    type: String,
+    required: true,
+  },
 });
 </script>
 
 <template>
-    <Head title="Welcome" />
+  <Head title="Welcome" />
+  <div class="relative sm:flex sm:items-center sm:justify-between p-8">
+    <h1 class="text-xl font-black uppercase">
+      Tambayan<span class="text-orange-500">Thoughts</span>
+    </h1>
 
-    <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
-    >
-        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
-            <Link
-                v-if="$page.props.auth.user"
-                :href="route('dashboard')"
-                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                >Dashboard</Link
-            >
+    <div>
+      <div v-if="canLogin">
+        <Link
+          v-if="$page.props.auth.user"
+          :href="route('dashboard')"
+          class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm"
+        >
+          Dashboard
+        </Link>
 
-            <template v-else>
-                <Link
-                    :href="route('login')"
-                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                    >Log in</Link
-                >
-
-                <Link
-                    v-if="canRegister"
-                    :href="route('register')"
-                    class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                    >Register</Link
-                >
-            </template>
-        </div>
-        <h1 class="text-4xl font-black">Blog Website</h1>
-      
+       
+        <template v-else>
+            <div class="flex gap-5">
+              <Link
+                :href="route('login')"
+                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm"
+              >
+                Blogs
+              </Link>
+              <Link
+                :href="route('login')"
+                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm"
+              >
+                Log in
+              </Link>
+  
+              <Link
+                v-if="canRegister"
+                :href="route('register')"
+                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm"
+              >
+                Register
+              </Link>
+            </div>
+          </template>
+      </div>
+    
     </div>
+   
+  </div>
+  <h1 class="p-8 mt-36 text-4xl font-bold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, alias quisquam aliquam tempora tempore velit? Reiciendis enim recusandae neque similique.</h1>
 </template>
-
